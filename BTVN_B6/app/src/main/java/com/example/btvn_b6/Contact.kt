9f71da0a839d1aa4685739d0e3106626427d07ac
Trần Thị Trà Mi <108862933+tmee2101@users.noreply.github.com>
2023-05-21 14:02:31 +0700

    Delete BTVN_B6/app/src/main directory

--- BTVN_B6/app/src/main/java/com/example/btvn_b6/Contact.kt
+++
@@ -1,5 +0,0 @@
-package com.example.btvn_b6
-
-import android.media.Image
-
-data class Contact(private val name: String, private val phone: String, private val image: Image)
--- BTVN_B6/app/src/main/java/com/example/btvn_b6/UserAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.btvn_b6
-
-class UserAdapter {
-}
